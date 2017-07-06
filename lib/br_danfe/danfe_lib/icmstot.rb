e0de8b2b6f0d75669c01ec5440d1611453538c3a
Glauber Santana <glauber.santana@tivit.com.br>
2017-07-06 12:14:54 -0300

    Considera a quebra em partes dos impostos

--- lib/br_danfe/danfe_lib/icmstot.rb
+++ lib/br_danfe/danfe_lib/icmstot.rb
@@ -16,7 +16,7 @@ module BrDanfe
         pdf.lnumeric 0.85, 3.46, 14.09, 13.63, xml, "ICMSTot/vIPI"
         pdf.lnumeric 0.85, 3.27, 17.55, 13.63, xml, "ICMSTot/vNF", :style => :bold
 
-        valor_aprox_impostos = xml.css("obsCont[xCampo='IMPOSTOS']/xTexto").text
+        valor_aprox_impostos = xml.css("obsCont[xCampo='IMPOSTOS']/xTexto").map(&:text).join
 
         pdf.ibox 0.85, 20.56, 0.25, 14.48, "VALOR APROXIMADO DOS IMPOSTOS", valor_aprox_impostos, { :size => 8 }
       end
--- lib/br_danfe/version.rb
+++ lib/br_danfe/version.rb
@@ -1,3 +1,3 @@
 module BrDanfe
-  VERSION = "0.0.2.4"
+  VERSION = "0.0.2.5
 end
